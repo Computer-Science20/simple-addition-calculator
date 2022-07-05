5be93021f527fed4bbf9d94d37d8c1c3975958a6
dhiyashah <dhiyashah2005@gmail.com>
2022-07-05 10:31:33 -0600

    Update

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hi");
+// Simple Addition Calculator
+
+// Add Event Listener to the Button
+document.getElementById("btn").addEventListener("click", calcTotal);
+
+// Event Function
+function calcTotal() {
+    // Input
+    let num1 = +document.getElementById("num1").value;
+    let num2 = +document.getElementById("num2").value;
+
+    // Process
+    let total = num1 + num2;
+
+    // Output
+    document.getElementById("total").innerHTML = total;
+}
